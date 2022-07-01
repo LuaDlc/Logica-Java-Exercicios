97691fe4420c99d0e9f952d1805dab1b91fb8d52
LuaDlc <luadiaslc@gmail.com>
2022-07-01 14:14:50 -0300

    first commit and exercise 1

---
+++ src/exercises/logica/java/Exercise01.java
@@ -0,0 +1,22 @@
+package exercises.logica.java;
+
+import java.util.Scanner;
+
+public class Exercise01 {
+    public static void main(String[] args) {
+        int num1= 0;
+        int num2 = 0;
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro numero: ");
+        num1 = leitor.nextInt();
+
+        System.out.println("Digite o segundo numero: ");
+        num2 = leitor.nextInt();
+
+        int soma = num1 + num2;
+        System.out.println("A soma entre os numeros e:" + soma);
+    }
+
+
+}
